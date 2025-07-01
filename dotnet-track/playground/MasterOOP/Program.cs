91712a611e6daa00aba2d909be9eaec28e1eab5f
hady <hady@issi.net>
2025-07-01 18:37:00 +0300

    x

--- dotnet-track/playground/MasterOOP/Program.cs
+++ dotnet-track/playground/MasterOOP/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Hady!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Wafa!");
