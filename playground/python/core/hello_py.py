56f9266156beb10701efe2cc0a187127b78b0143
hady <97hadywafa@gmail.com>
2026-06-13 17:40:48 +0400

    save

--- playground/python/core/hello_py.py
+++ playground/python/core/hello_py.py
@@ -1,6 +1,48 @@
+
+## Looping
+names: list[str] = [
+    "hady",
+    "ahmed",
+    "mohamed",
+    "ali",
+    "ibrahimovic",
+    "messi",
+    "ronaldo",
+    "neymar",
+]
+
+### loop approach 1
+longest_name1 = []
+for name in names:
+    if len(name) > 7:
+        longest_name1.append(name)
+print(longest_name1)
+
+### loop approach 2
+longest_name2 = [name for name in names if len(name) > 7]
+print(longest_name2)
+
+# basic functions
 def say_hello(name: str) -> str:
     print(f"Hello, {name}")
 
 
+# empty function
+def fun1(): ...
+
+
+# class
+class human:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __str__(self) -> str:
+        return f"My name is {self.name}"
+
+
+# if the file is run directly (not imported as a module) then the main function is executed
 if __name__ == "__main__":
-    say_hello("John Doe")
+    print("=================== main function =====================")
+    say_hello("John")
+    fun1()
+    print(human("John"))
---
+++ playground/python/core/main.py
@@ -0,0 +1,4 @@
+from hello_py import say_hello
+
+if __name__ == "__main__":
+    say_hello("Hady")
